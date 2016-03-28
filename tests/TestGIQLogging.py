09e57b535ebde9bda27ffe55447467db345ef9a5
Jesse Adametz <jesse@graphiq.com>
2016-03-28 11:51:34 -0700

    Tests: initial unittests instead of doctests

---
+++ tests/TestGIQLogging.py
@@ -0,0 +1,26 @@
+import logging
+import unittest
+
+import GIQLogging
+from logstash_formatter import LogstashFormatterV1
+
+
+class TestGIQLogging(unittest.TestCase):
+    def test_basic_init_is_debug_level(self):
+        logger = GIQLogging.init(logger_name='test1')
+
+        self.assertEqual(logger.level, logging.DEBUG)
+
+    def test_basic_init_formatter_is_LogstashFormatterV1(self):
+        logger = GIQLogging.init(logger_name='test2')
+        self.assertIsInstance(logger.handlers[0].formatter, LogstashFormatterV1)
+
+    def test_init_with_extra_fields(self):
+        extra_fields = {'foo': 'bar', 'zip': 'zap'}
+        logger = GIQLogging.init(extra_fields=extra_fields, logger_name='test3')
+        for key in extra_fields.keys():
+            self.assertIsNotNone(logger.handlers[0].formatter.defaults.get(key))
+
+
+if __name__ == '__main__':
+    unittest.main()
